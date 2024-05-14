4cf6fbab816a284d275da3f05569a8afbf41ee68
Pedram Elmi <pedram.elmi@gmail.com>
2024-05-14 23:53:52 +0330

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -21,3 +21,7 @@ def get_random_ingredients(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+    
+def new_func():
+    """New Function doing nothing"""
+    pass
